a1e3bd843be3b4f4fc3fd957d399980cd0253ad1
Guru9705 <96834192+Guru9705@users.noreply.github.com>
2022-02-05 01:56:26 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Counter from './Counter';
+import Mobile from './Mobile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Counter value={1}/>
+      {/* <Counter value={1}/> */}
+      <Mobile></Mobile>
     </div>
   );
 }
